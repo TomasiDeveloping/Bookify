8276a95f1d175d213033d99c3a3263420ee968df
Tomasi - Developing <info@tomasi-developing.ch>
2024-02-08 10:26:31 +0100

    Projektdateien hinzufügen.

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity(Guid id)
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public Guid Id { get; init; } = id;
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvent()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvents(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,135 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    private Booking(
+        Guid id,
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amenitiesUpChange,
+        Money totalPrise,
+        BookingStatus status,
+        DateTime createdOnUtc)
+        : base(id)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpChange = amenitiesUpChange;
+        TotalPrise = totalPrise;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public Guid ApartmentId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateRange Duration { get; }
+
+    public Money PriceForPeriod { get; private set; }
+
+    public Money CleaningFee { get; private set; }
+
+    public Money AmenitiesUpChange { get; private set; }
+
+    public Money TotalPrise { get; private set; }
+
+    public BookingStatus Status { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? ConfirmedOnUtc { get; private set; }
+
+    public DateTime? RejectedOnUtc { get; private set; }
+
+    public DateTime? CompletedOnUtc { get; private set; }
+
+    public DateTime? CancelledOnUtc { get; private set; }
+
+    public static Booking Reserve(Apartment apartment, Guid userId, DateRange duration, DateTime utcNow,
+        PricingService pricingService)
+    {
+        var pricingDetails = pricingService.CalculatePrise(apartment, duration);
+        var booking = new Booking(
+            Guid.NewGuid(),
+            apartment.Id,
+            userId,
+            duration,
+            pricingDetails.PriceForPeriod,
+            pricingDetails.CleaningFee,
+            pricingDetails.AmenitiesUpCharge,
+            pricingDetails.TotalPrice,
+            BookingStatus.Reserved,
+            utcNow
+        );
+
+        booking.RaiseDomainEvents(new BookingReservedDomainEvent(booking.Id));
+
+        apartment.LastBookOnUtc = utcNow;
+
+        return booking;
+    }
+
+    public Result Confirm(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved) return Result.Failure(BookingErrors.NotReserved);
+
+        Status = BookingStatus.Confirmed;
+        ConfirmedOnUtc = utcNow;
+
+        RaiseDomainEvents(new BookingConfirmedDomainEvent(Id));
+
+        return Result.Success();
+    }
+
+    public Result Reject(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved) return Result.Failure(BookingErrors.NotReserved);
+
+        Status = BookingStatus.Rejected;
+        RejectedOnUtc = utcNow;
+
+        RaiseDomainEvents(new BookingRejectedDomainEvent(Id));
+
+        return Result.Success();
+    }
+
+    public Result Complete(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Confirmed) return Result.Failure(BookingErrors.NotConfirmed);
+
+        Status = BookingStatus.Completed;
+        CompletedOnUtc = utcNow;
+
+        RaiseDomainEvents(new BookingCompletedDomainEvent(Id));
+
+        return Result.Success();
+    }
+
+    public Result Cancel(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Confirmed) return Result.Failure(BookingErrors.NotConfirmed);
+
+        var currentDate = DateOnly.FromDateTime(utcNow);
+
+        if (currentDate > Duration.Start) return Result.Failure(BookingErrors.AlreadyStarted);
+
+        Status = BookingStatus.Cancelled;
+        CancelledOnUtc = utcNow;
+
+        RaiseDomainEvents(new BookingCancelledDomainEvent(Id));
+
+        return Result.Success();
+    }
+}
